fd6d1c28aa2f4606c874fd1c8cb5bc512a3f6c75
archepro84 <archepro84@gmail.com>
2022-02-01 10:17:47 +0900

    feat: Add board C R controller

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import {Board, BoardStatus} from "./boards.model";
+import {v1 as uuid} from 'uuid'; // v1 Version
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+    private boards: Board[] = [];
+
+    getAllBoards(): Board[] {
+        return this.boards;
+    }
+
+    createBoard(title: string, description: string) {
+        const board: Board = {
+            id: uuid(),
+            title,
+            description,
+            status: BoardStatus.PUBLIC,
+        }
+
+        this.boards.push(board);
+        return board;
+    }
+}
