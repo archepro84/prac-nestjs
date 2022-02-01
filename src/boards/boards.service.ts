9f8285acb937d3cec3d8aafcfde942b0ec3b7b91
archepro84 <archepro84@gmail.com>
2022-02-01 23:27:35 +0900

    feat: Add boards CRUD

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,24 +1,41 @@
-import {Injectable} from '@nestjs/common';
-import {Board, BoardStatus} from "./boards.model";
-import {v1 as uuid} from 'uuid'; // v1 Version
+import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './boards.model';
+import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto'; // v1 Version
 
 @Injectable()
 export class BoardsService {
-    private boards: Board[] = [];
+  private boards: Board[] = [];
 
-    getAllBoards(): Board[] {
-        return this.boards;
-    }
+  createBoard(createBoardDto: CreateBoardDto) {
+    const { title, description } = createBoardDto;
 
-    createBoard(title: string, description: string) {
-        const board: Board = {
-            id: uuid(),
-            title,
-            description,
-            status: BoardStatus.PUBLIC,
-        }
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
 
-        this.boards.push(board);
-        return board;
-    }
+    this.boards.push(board);
+    return board;
+  }
+
+  getAllBoards(): Board[] {
+    return this.boards;
+  }
+
+  getBoardById(id: string): Board {
+    return this.boards.find((board) => board.id === id);
+  }
+
+  updateBoardStatus(id: string, status: BoardStatus): Board {
+    const board = this.getBoardById(id);
+    board.status = status;
+    return board;
+  }
+
+  deleteBoard(id: string): void {
+    this.boards = this.boards.filter((board) => board.id !== id);
+  }
 }
