7b48624292302ef14d29bc39300e30f8089f5723
archepro84 <archepro84@gmail.com>
2022-02-01 23:28:09 +0900

    refactor: Reformat eslint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,7 @@
-import {Module} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 
 @Module({
-    imports: [BoardsModule],
+  imports: [BoardsModule],
 })
-export class AppModule {
-}
+export class AppModule {}
