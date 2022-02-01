3ce11f6530d386a904b216cc889ca84acd9131e3
archepro84 <archepro84@gmail.com>
2022-02-02 00:16:22 +0900

    chore: Add logger pacakge winston

---
+++ src/utiles/logger.ts
@@ -0,0 +1,29 @@
+import { createLogger, transports, format } from 'winston';
+
+interface TransformableInfo {
+  level: string;
+  message: string;
+
+  [key: string]: any;
+}
+
+const logger = createLogger({
+  transports: [
+    new transports.Console({
+      level: 'debug',
+      format: format.combine(
+        format.label({ label: '[my-server]' }),
+        format.timestamp({
+          format: 'YYYY-MM-DD HH:mm:ss',
+        }),
+        format.colorize(),
+        format.printf(
+          (info: TransformableInfo) =>
+            `${info.timestamp} - ${info.level}: ${info.label} ${info.message}`,
+        ),
+      ),
+    }),
+  ],
+});
+
+export { logger };
