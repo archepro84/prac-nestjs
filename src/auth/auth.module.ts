b0aff2f6038a527770f3f921a1d7e2a976f1a9c2
archepro84 <archepro84@gmail.com>
2022-02-03 08:49:51 +0900

    feat: Add auth module

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'Secret1234',
+      signOptions: {
+        expiresIn: 60 * 60,
+      },
+    }),
+    TypeOrmModule.forFeature([UserRepository]),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy], //Auth Module에서 JwtStrategy를 사용할 수 있게 등록
+  exports: [JwtStrategy, PassportModule], // JwtStrategy, PassportModule을 다른 모듈에서 사용할 수 있도록 등록
+})
+export class AuthModule {}
