6f0a87114e1fe011155879c4897548b3dc4eeda6
archepro84 <archepro84@gmail.com>
2022-06-21 00:27:43 +0900

    feat: Add Users resource from modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,18 @@ import { BoardsModule } from './modules/boards/boards.module';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './modules/users/users.controller';
+import { UsersModule } from './modules/users/users.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule, AuthModule],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+    BoardsModule,
+    AuthModule,
+    UsersModule,
+  ],
+  controllers: [AppController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
