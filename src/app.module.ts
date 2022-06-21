df70404ff1e1332a039ddd9d289b64e6168522c5
archepro84 <archepro84@gmail.com>
2022-06-21 09:07:09 +0900

    feat: Add Config module base code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeORMConfig } from './configs/typeorm.config';
+import { ConfigModuleOptions } from '@nestjs/config';
+import { typeORMConfig } from './config/typeorm.config';
 import { BoardsModule } from './modules/boards/boards.module';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
@@ -8,12 +9,24 @@ import { AppService } from './app.service';
 import { UsersController } from './modules/users/users.controller';
 import { UsersModule } from './modules/users/users.module';
 
+// const configModuleOption: ConfigModuleOptions = {
+//   envFilePath: [
+//     `${__dirname}/config/env/.${
+//       process.env.NODE_ENV ? process.env.NODE_ENV : 'development'
+//     }.env`,
+//   ],
+//   load: [emailConfig],
+//   isGlobal: true,
+// };
+
 @Module({
   imports: [
+    // ConfigModule.forRoot(configModuleOption),
     TypeOrmModule.forRoot(typeORMConfig),
     BoardsModule,
     AuthModule,
     UsersModule,
+    // ConfigModule,
   ],
   controllers: [AppController],
   providers: [AppService],
