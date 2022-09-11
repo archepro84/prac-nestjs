9a97c320abeb5caa604ef5794d59cb0530e6346c
ArchaeG <archepro84@gmail.com>
2022-09-12 08:00:15 +0900

    feat: Add return typing for posts domain

--- nest-sequelize/src/posts/posts.controller.ts
+++ nest-sequelize/src/posts/posts.controller.ts
@@ -1,18 +1,19 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { AddPostDto } from './posts.dto';
+import { Post as PostEntity } from './posts.entity';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Post('/')
-  addPost(@Body() addPostDto: AddPostDto) {
+  addPost(@Body() addPostDto: AddPostDto): Promise<PostEntity> {
     return this.postsService.addPost(addPostDto);
   }
 
   @Get('/')
-  findAll() {
+  findAll(): Promise<PostEntity[]> {
     return this.postsService.findAll();
   }
 }
--- nest-sequelize/src/posts/posts.module.ts
+++ nest-sequelize/src/posts/posts.module.ts
@@ -3,10 +3,11 @@ import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
 import { DatabaseModule } from '../database/database.module';
 import { PostsProviders } from './posts.providers';
+import { PostsRepository } from './posts.repository';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [PostsController],
-  providers: [PostsService, ...PostsProviders],
+  providers: [PostsService, PostsRepository, ...PostsProviders],
 })
 export class PostsModule {}
