b70f1ab5a39de94bde0976501c5e8b57a8c10845
ArchaeG <archepro84@gmail.com>
2022-09-12 09:14:38 +0900

    feat: Add hard code to dotenv variables

--- nest-sequelize/src/database/database.providers.ts
+++ nest-sequelize/src/database/database.providers.ts
@@ -1,18 +1,12 @@
 import { Sequelize } from 'sequelize-typescript';
 import { Post } from '../posts/posts.entity';
+import { databaseConfig } from '../config/configs';
 
 export const DatabaseProviders = [
   {
     provide: 'SEQUELIZE',
     useFactory: async () => {
-      const sequelize = new Sequelize({
-        dialect: 'mysql',
-        host: '127.0.0.1',
-        port: 3306,
-        username: 'root',
-        password: '1234',
-        database: 'nest_sequelize',
-      });
+      const sequelize = new Sequelize(databaseConfig);
       sequelize.addModels([Post]);
       await sequelize.sync();
       return sequelize;
--- nest-sequelize/src/main.ts
+++ nest-sequelize/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { appConfig } from './config/configs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = appConfig.port;
+
+  await app.listen(port);
 }
+
 bootstrap();
